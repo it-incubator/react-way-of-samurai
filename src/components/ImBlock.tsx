7c2c97d0c32cddd0e00475acb7542e5f970d9364
dariametelitsa <109726679+dariametelitsa@users.noreply.github.com>
2025-04-10 12:44:46 +0300

    Way 0-10: complete flow react-way-of-samurai 0-10 (#1)
    * feat: complete flow react-way-of-samurai 0-10
    
    * feat: complete flow react-way-of-samurai 0-10
    
    * refactor: add .prettierrc, set up eslint.config.js
    
    * fix: README.md
    
    * WAY-0-10 feat: add version
    
    ---------
    
    Co-authored-by: Veta-s <elsava1999@gmail.com>

---
+++ src/components/ImBlock.tsx
@@ -0,0 +1,13 @@
+export const ImBlock = () => {
+  return (
+    <>
+      <h1>Список важного:</h1>
+      <ul>
+        <li>То</li>
+        <li>Сё</li>
+        <li>5</li>
+        <li>10</li>
+      </ul>
+    </>
+  )
+}
